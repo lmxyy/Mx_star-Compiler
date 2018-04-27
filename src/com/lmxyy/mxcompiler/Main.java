4f090f85288cea375e2f836a9c0a80074b7960d7
Lmxyy <664789531@qq.com>
2018-04-27 15:24:27 +0800

    comment the debug information

--- src/com/lmxyy/mxcompiler/Main.java
+++ src/com/lmxyy/mxcompiler/Main.java
@@ -27,8 +27,8 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        String inFile = "/Users/limuyang/Desktop/Mx_star-Compiler/testcase/686.mx", outFile = null;
-        // String inFile = null,outFile = null;
+        // String inFile = "/Users/limuyang/Desktop/Mx_star-Compiler/testcase/680.mx", outFile = null;
+        String inFile = null,outFile = null;
         boolean isPrintHelp = false, isPrintVersion = false, isPrintConfig = false;
         for (int i = 0; i < args.length; ++i) {
             String arg = args[i];
