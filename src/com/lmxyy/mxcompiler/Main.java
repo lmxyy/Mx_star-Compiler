1c74945c2aaeb36fa2397a888703c19338e87fed
Lmxyy <664789531@qq.com>
2018-06-07 13:42:12 +0800

    modified Main

--- src/com/lmxyy/mxcompiler/Main.java
+++ src/com/lmxyy/mxcompiler/Main.java
@@ -2,6 +2,7 @@ package com.lmxyy.mxcompiler;
 
 import com.lmxyy.mxcompiler.compiler.Compiler;
 import com.lmxyy.mxcompiler.utils.LLIRInterpreter;
+import com.lmxyy.mxcompiler.utils.WarningInfo;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
@@ -9,14 +10,13 @@ import java.util.Date;
 import java.util.Locale;
 
 public class Main {
-    private static final String Ver = "0.0.0";
+    private static final String Ver = "0.0.1";
 
     private static void printHelp() {
         System.out.println("Usage: mxcompiler [options] file...");
         System.out.println("Options:");
         System.out.println("  -h, --help                    Display this information");
         System.out.println("  -v, --version                 Display compiler version information");
-        System.out.println("  -c, --config                  Display compiler configurations");
         System.out.println("  -o <file>                     Place the output into <file>");
     }
 
@@ -30,10 +30,14 @@ public class Main {
         System.exit(1);
     }
 
+    private static void printWarning() {
+        WarningInfo.msgs.forEach(msg->{ System.err.println(msg); });
+    }
+
     public static void main(String[] args) throws Exception {
-//         String inFile = "/Users/limuyang/Desktop/Mx_star-Compiler/testcase/939.mx", outFile = null;
+//         String inFile = "/Users/limuyang/Desktop/Mx_star-Compiler/testcase/953.mx", outFile = null;
         String inFile = null,outFile = null;
-        boolean isPrintHelp = false, isPrintVersion = false, isPrintConfig = false;
+        boolean isPrintHelp = false, isPrintVersion = false, isPrintWarning = false;
         for (int i = 0; i < args.length; ++i) {
             String arg = args[i];
             switch (arg) {
@@ -47,14 +51,13 @@ public class Main {
                     isPrintVersion = true;
                     break;
 
-                case "-c":
-                case "--config":
-                    isPrintConfig = true;
-                    break;
-
                 case "-o":
                     if (i + 1 < args.length) outFile = args[++i];
                     else errorArgs();
+                    break;
+
+                case "-wall":
+                    isPrintWarning = true;
                     break;
 
                 default:
@@ -64,7 +67,6 @@ public class Main {
         }
         if (isPrintVersion) printVersion();
         if (isPrintHelp) printHelp();
-        // if (isPrintConfig) printConfig();
 
         InputStream inS;
         PrintStream outS;
@@ -81,6 +83,8 @@ public class Main {
                 System.exit(1);
             }
             else {
+                if (isPrintWarning)
+                    printWarning();
                 System.err.println("Compilation finished at "+formatter.format(new Date()));
             }
         }
--- src/com/lmxyy/mxcompiler/utils/CompilerOption.java
+++ src/com/lmxyy/mxcompiler/utils/CompilerOption.java
@@ -5,17 +5,6 @@ public class CompilerOption {
     private static final int SIZE_BOOL = 1;
     private static final int SIZE_POINTER = 8;
 
-    public static boolean ifPrintAST = false;
-    public static boolean ifPrintRawIR = false;
-    public static boolean ifPrintSSAIR = false;
-    public static boolean ifPrintIRAfterInline = false;
-    public static boolean enableWarning = true;
-    public static boolean enableInline = true;
-    public static boolean enableSSA = true;
-    public static boolean eliminateDeadCode = true;
-    public static boolean simpleConstantPropagate = true;
-    public static String registerAllocator = "color";
-
     public static int getSizeInt() {
         return SIZE_INT;
     }
